9878717ac4e09d17929e2dd0a1996cd925c963ae
yls <2689623200@qq.com>
2020-08-23 21:10:34 +0800

    修改配置文件

--- yls_base_core/src/main/java/com/yls/core/page/MyPageRequest.java
+++ yls_base_core/src/main/java/com/yls/core/page/MyPageRequest.java
@@ -1,17 +1,27 @@
 package com.yls.core.page;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 分页请求
- * @author Louis
- * @date Jan 12, 2019
+ * 功能描述:
+ * 〈分页请求,参数采用键值对封装〉
+ * @author : yls
+ * @date : 2020/07/19 10:59
  */
 public class MyPageRequest {
-	//当前页码
+	/**
+	 * 当前页码
+	 */
 	private int pageNum = 1;
-	//每页数量
+	/**
+	 * 每页数量
+	 */
 	private int pageSize = 10;
-	//查询参数
-	//private Map<String, Object> params = new HashMap<>();
+	/**
+	 * 查询参数
+	 */
+	private Map<String, Object> params = new HashMap<>();
 	
 	public int getPageNum() {
 		return pageNum - 1;
@@ -25,22 +35,19 @@ public class MyPageRequest {
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 	}
-//	public Map<String, Object> getParams() {
-//		return params;
-//	}
-//	public void setParams(Map<String, Object> params) {
-//		this.params = params;
-//	}
-//	public Object getParam(String key) {
-//		return getParams().get(key);
-//	}
+	public Map<String, Object> getParams() {
+		return params;
+	}
+	public void setParams(Map<String, Object> params) {
+		this.params = params;
+	}
 
 	@Override
 	public String toString() {
-		return "PageRequest{" +
+		return "MyPageRequest{" +
 				"pageNum=" + pageNum +
 				", pageSize=" + pageSize +
-				//", params=" + params +
+				", params=" + params +
 				'}';
 	}
 }
