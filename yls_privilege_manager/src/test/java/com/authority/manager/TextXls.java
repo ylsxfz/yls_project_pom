31ba475890663d3ad2115084085339e2e7316337
yls <2689623200@qq.com>
2020-10-18 20:37:22 +0800

    利用mybatis自动注入创建时间、创建人、更新时间、更新人

--- yls_privilege_manager/src/test/java/com/authority/manager/TextXls.java
+++ yls_privilege_manager/src/test/java/com/authority/manager/TextXls.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Auther: yls
@@ -19,7 +20,7 @@ import java.util.*;
 public class TextXls {
     public static void main(String[] args) {
         //创建存储数据
-        Map<String, Map<String,Integer>> contentMap = new HashMap<>();
+        Map<String, Map<String, Integer>> contentMap = new HashMap<>();
         try {
             jxl.Workbook wb = null;
             InputStream is = new FileInputStream("D:\\workspace\\demo.xls");
@@ -35,21 +36,21 @@ public class TextXls {
                 String contentsFirst = cells[0].getContents();
                 String contentsTwo = cells[1].getContents();
 
-                if (!"0".equals(contentsFirst)&& !"".equals(contentsFirst)){
-                    newTemp = j+"_"+contentsFirst;
+                if (!"0".equals(contentsFirst) && !"".equals(contentsFirst)) {
+                    newTemp = j + "_" + contentsFirst;
                 }
 
-                if (contentMap.containsKey(newTemp)){
+                if (contentMap.containsKey(newTemp)) {
                     Map<String, Integer> stringIntegerMap = contentMap.get(newTemp);
-                    if (stringIntegerMap.containsKey(contentsTwo)){
-                        stringIntegerMap.put(contentsTwo,stringIntegerMap.get(contentsTwo)+1);
-                    }else {
-                        stringIntegerMap.put(contentsTwo,1);
+                    if (stringIntegerMap.containsKey(contentsTwo)) {
+                        stringIntegerMap.put(contentsTwo, stringIntegerMap.get(contentsTwo) + 1);
+                    } else {
+                        stringIntegerMap.put(contentsTwo, 1);
                     }
-                }else {
+                } else {
                     Map<String, Integer> stringIntegerMap = new HashMap<>();
-                    stringIntegerMap.put(contentsTwo,1);
-                    contentMap.put(newTemp,stringIntegerMap);
+                    stringIntegerMap.put(contentsTwo, 1);
+                    contentMap.put(newTemp, stringIntegerMap);
                 }
 
                 // 打印每一行
@@ -64,43 +65,60 @@ public class TextXls {
         }
         Map<String, Map<String, Integer>> resultContentMap = sortMapKey(contentMap);
 
-        System.out.println("第一列标记，第二列的值，去重数量，不去重数量");
-        resultContentMap.forEach((key,value)->{
+
+//        resultContentMap.forEach((key,value)->{
+//            Collection<Integer> values = value.values();
+//            int sum = values.stream().mapToInt(Integer::intValue).sum();
+//            System.err.println(key+":"+value.size()+":"+sum);
+//        });
+        /**
+         * 重复次数大于指定的数
+         */
+        int compareNumber = 2;
+        System.out.println("第一列标记，第二列的值，去重数量，不去重数量，重复次数大于"+compareNumber);
+        resultContentMap.forEach((key, value) -> {
             Collection<Integer> values = value.values();
             int sum = values.stream().mapToInt(Integer::intValue).sum();
-            System.err.println(key+":"+value.size()+":"+sum);
-        });
+            AtomicInteger totalCount = new AtomicInteger();
+            // 重复数据量大于4
+            value.forEach((tempKey, tempValue) -> {
+                if (tempValue > compareNumber) {
+                    totalCount.addAndGet(1);
+                }
+            });
+            System.err.println(key + "," + value.size() + "," + sum+","+totalCount.toString());
 
+        });
         System.out.println();
         /**
          * 重复次数大于指定的数
          */
-        int compareNum = 2;
-        resultContentMap.forEach((key,value)->{
-            Collection<Integer> values = value.values();
-            int sum = values.stream().mapToInt(Integer::intValue).sum();
-            // 重复数据量大于4
-            value.forEach((tempKey,tempValue)->{
-                if (tempValue>compareNum){
-                    System.out.println("重复次数大于"+compareNum+"："+key+"=>"+tempKey+"=>"+tempValue);
-                }
-            });
-        });
+//        int compareNum = 2;
+//        resultContentMap.forEach((key,value)->{
+//            Collection<Integer> values = value.values();
+//            int sum = values.stream().mapToInt(Integer::intValue).sum();
+//            // 重复数据量大于4
+//            value.forEach((tempKey,tempValue)->{
+//                if (tempValue>compareNum){
+//                    System.out.println("重复次数大于"+compareNum+"："+key+"=>"+tempKey+"=>"+tempValue);
+//                }
+//            });
+//        });
     }
 
     /**
-     * @Description 生成UUID，全为小写
      * @return java.lang.String
+     * @Description 生成UUID，全为小写
      **/
     public static String uuidUpperCase() {
         return UUID.randomUUID().toString().toLowerCase().replaceAll("-", "");
     }
 
 
-    public static Map<String, Map<String, Integer>> sortMapKey(Map<String, Map<String,Integer>> map) {
-        List<Map.Entry<String, Map<String,Integer>>> list = new ArrayList(map.entrySet());
+    public static Map<String, Map<String, Integer>> sortMapKey(Map<String, Map<String, Integer>> map) {
+        List<Map.Entry<String, Map<String, Integer>>> list = new ArrayList(map.entrySet());
         list.sort((o1, o2) -> Integer.parseInt(o1.getKey().split("_")[0]) - Integer.parseInt(o2.getKey().split("_")[0]));
-        Map<String, Map<String,Integer>> linkedMap = new LinkedHashMap<>();
+        Map<String, Map<String, Integer>> linkedMap = new LinkedHashMap<>();
         for (Map.Entry<String, Map<String, Integer>> entry : list) {
             linkedMap.put(entry.getKey(), entry.getValue());
         }
