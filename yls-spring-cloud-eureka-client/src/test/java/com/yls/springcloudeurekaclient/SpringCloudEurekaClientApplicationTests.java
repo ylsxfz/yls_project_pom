9a0b4b88e0cfe2544eff400b7da3213d645b1ba7
yls <2689623200@qq.com>
2020-11-11 17:08:33 +0800

    添加spring-cloud-eureka-client

---
+++ yls-spring-cloud-eureka-client/src/test/java/com/yls/springcloudeurekaclient/SpringCloudEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yls.springcloudeurekaclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
