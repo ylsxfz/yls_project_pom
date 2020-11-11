218b4f863cfefdbe53093a7d5b3dea0bb2308ae3
yls <2689623200@qq.com>
2020-11-11 22:11:56 +0800

    添加eureka高可用配置

---
+++ yls-spring-cloud-eureka-server-second/src/test/java/com/yls/ylsspringcloudeurekaserversecond/YlsSpringCloudEurekaServerSecondApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yls.ylsspringcloudeurekaserversecond;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlsSpringCloudEurekaServerSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
