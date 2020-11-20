6eb6fa9abd023c4430128aa690540f525464cfd4
yls <2689623200@qq.com>
2020-11-20 17:57:05 +0800

    添加nacos服务

--- yls-cloud-nacos-config/src/main/java/com/yls/ylscloudnacos/YlsCloudNacosApplication.java
+++ yls-cloud-nacos-config/src/main/java/com/yls/ylscloudnacos/YlsCloudNacosApplication.java
@@ -2,8 +2,10 @@ package com.yls.ylscloudnacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class YlsCloudNacosApplication {
 
     public static void main(String[] args) {
